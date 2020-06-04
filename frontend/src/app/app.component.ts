335a6b6e90007aae538f8d4dba5ed0bbae098854
Pradeep Verma <pradeep1.verma@paytm.com>
2020-06-04 14:34:00 +0530

    frontend code added

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Green Link'
+  crops = [{name:'Sugar Cane'}, {name:'Wheat'}];
+}
